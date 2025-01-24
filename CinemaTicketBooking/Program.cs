7945d3e7f9c92d2371104418d04b474767eec37a
Jose <pedrojcunha7@hotmail.com>
2025-01-24 15:31:01 +0100

    feat: setup login for admin with password verification

--- CinemaTicketBooking/Program.cs
+++ CinemaTicketBooking/Program.cs
@@ -2,7 +2,50 @@
 using dotenv.net;
 
 class Program{
-    static void Main(){
-        Console.WriteLine("bonjour");
+    static void Main()
+    {
+        LoginMenu();
     }
+    static void LoginMenu()
+    {
+        Console.WriteLine("Bienvenue dans notre cinema!");
+        Console.WriteLine("1. Admin");
+        Console.WriteLine("2. Visiteur");
+
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                AdminLogin();
+                break;
+            case "2":
+                //GuessMenu();
+                break;
+        }
+    }
+    static void AdminLogin(){
+        Console.WriteLine("Identifiez-vous!");
+        DotEnv.Load();
+
+        string? password = Console.ReadLine();
+        string? verificationPassword = Environment.GetEnvironmentVariable("password");
+            if (password != verificationPassword)
+            {
+                Console.WriteLine("Mot de passe incorrect");
+            }
+            else
+            {
+                Console.WriteLine("Mot de passe correct");
+                //AdminMenu();
+                return;
+            }
+    }
+}
+
+class Films{
+    string Name {get; set;}
+    int Price {get; set;}
+    string Description {get; set;}
+    string Category {get; set;}
 }
